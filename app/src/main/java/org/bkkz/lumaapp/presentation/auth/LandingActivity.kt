91cea81c946b3d3af23a9d422419aee0ebcfdab5
Phrit Nakgate <phrit.nakg@cdg.co.th>
2025-09-09 19:16:36 +0700

    feat(login): login page layout (incomplete)

--- app/src/main/java/org/bkkz/lumaapp/presentation/auth/LandingActivity.kt
+++ app/src/main/java/org/bkkz/lumaapp/presentation/auth/LandingActivity.kt
@@ -1,17 +1,30 @@
 package org.bkkz.lumaapp.presentation.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import org.bkkz.lumaapp.R
+import org.bkkz.lumaapp.presentation.auth.login.LoginActivity
 
 class LandingActivity : AppCompatActivity() {
+
+    private lateinit var getStartedBtn : ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_landing)
+
+        getStartedBtn = findViewById(R.id.constraintlayout_landing_btn)
+        getStartedBtn.setOnClickListener {
+            val intent = Intent(this@LandingActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.landing_page)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
