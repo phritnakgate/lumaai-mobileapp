110982bea7d6ca8639c7d462d42c6926f63677a3
Phrit Nakgate <phrit.nakg@cdg.co.th>
2025-09-09 15:34:27 +0700

    chore(assets): add fonts and colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "org.bkkz.lumaapp"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
